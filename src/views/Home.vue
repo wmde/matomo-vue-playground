32faa5bd6b13804ad7831cf857155eaa8c8da539
Gabriel Birke <gabriel.birke@posteo.net>
2021-07-07 17:52:30 +0200

    Rewrite form for new test
    Test different form submissions for
    https://phabricator.wikimedia.org/T285945

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,37 +1,73 @@
 <template>
   <div class="home">
-    <h2>Choice Form:</h2>
-    <form id="choice" class="choiceForm">
-      <label>
-        <input type="radio" name="formtype" value="a" v-model="formtype" />
-        Form type A</label
-      >
-      <label>
-        <input type="radio" name="formtype" value="b" v-model="formtype" />
-        Form type B</label
-      >
-      <label>
-        <input type="radio" name="formtype" value="c" v-model="formtype" />
-        Form type C</label
-      >
+    <h2>Normal form with submit</h2>
+    <form id="form-with-integrated-submit" action="/about" method="get">
+      <div>
+        <label for="name1">Your Name</label>
+        <input id="name1" name="email" />
+      </div>
+      <div>
+        <label for="email1">Your email address</label>
+        <input id="email1" name="email" />
+      </div>
+      <button type="submit" name="submit" id="submit">Submit</button>
     </form>
-    <div class="displayFormId">
-      Current Matomo form id: <code>dynamic-form-{{ formtype }}</code>
+
+    <h2>Form with external submit button</h2>
+    <form id="form-with-external-submit" action="/about" method="get">
+      <div>
+        <label for="name2">Your Name</label>
+        <input id="name2" v-model="name" />
+      </div>
+      <div>
+        <label for="email2">Your email address</label>
+        <input id="email2" v-model="email" />
+      </div>
+      <input type="hidden" :value="name" name="name" />
+      <input type="hidden" :value="email" name="email" />
+    </form>
+    <form
+      id="submitting-form"
+      action="/about"
+      method="get"
+      @submit="submitExternalForm"
+    >
+      <input
+        type="hidden"
+        name="tripwire"
+        value="this-should-never-be-submitted"
+      />
+      <button type="submit" name="submit-external" id="submit-external">
+        Submit
+      </button>
+    </form>
+
+    <h2>Submit button with hidden fields</h2>
+    <form id="hidden-value-form" action="/about" method="get">
+      <input type="hidden" :value="name" name="name" />
+      <input type="hidden" :value="email" name="email" />
+      <button type="submit" name="submit-values" id="submit-values">
+        Submit
+      </button>
+    </form>
+
+    <div v-show="name === 'showform'">
+      <h2>Hidden Form</h2>
+      <form id="hidden-submit-form" action="/about" method="get">
+        <div>
+          <label for="name3">Your Name</label>
+          <input id="name3" name="email" />
+        </div>
+        <div>
+          <label for="email3">Your email address</label>
+          <input id="email3" name="email" />
+        </div>
+
+        <button type="submit" name="submit-values" id="submit-values">
+          Submit
+        </button>
+      </form>
     </div>
-    <h2>Dynamic Form</h2>
-    <form :id="`dynamic-form-${formtype}`" class="dynamicForm" action="/about">
-      <div>Shared input on all form types: <input name="name" /></div>
-      <div v-if="formtype == 'a'">
-        Special input on type A: <input name="special" />
-      </div>
-      <div v-if="formtype == 'b'">
-        Special input on type B: <input name="special" />
-      </div>
-      <div v-if="formtype !== 'unset'">
-        <button type="submit">Submit</button>
-      </div>
-      <div v-else>Please select a form type</div>
-    </form>
   </div>
 </template>
 
@@ -44,36 +80,43 @@ export default {
   name: "Home",
   data() {
     return {
-      formtype: "unset",
+      name: "",
+      email: "",
     };
   },
-  watch: {
-    formtype: function (oldFormtype, newFormtype) {
-      console.log("Form type changed", oldFormtype, newFormtype);
-      if (typeof _paq !== "undefined") {
-        // scan on every change, just to be sure Matomo picks up the new form id
-        console.log("Triggering form scan ...");
-        _paq.push(["FormAnalytics::scanForForms"]);
-      }
+  methods: {
+    submitExternalForm(e) {
+      e.preventDefault();
+      const formElement = document.getElementById("form-with-external-submit");
+      _paq.push(["FormAnalytics::trackFormSubmit", formElement]);
+      formElement.submit();
     },
   },
 };
 </script>
 
 <style>
-.choiceForm label {
-  display: block;
+label {
+  display: inline-block;
+  width: 150px;
 }
 
-.displayFormId {
-  padding: 2em;
-  background: lightgrey;
-  margin: 2em 0;
+form {
+  padding: 5px;
+  margin: 30px 0 0 0;
+  border: dotted blue 1px;
+  position: relative;
 }
-.dynamicForm div {
-  margin-bottom: 1em;
-}
-code {
-  font-weight: bold;
+
+form[id]:before {
+  position: absolute;
+  content: attr(id);
+  color: blue;
+  padding: 2px 3px;
+  border: dotted blue 1px;
+  top: -22px;
+  left: -1px;
+  background: white;
+  height: 24px;
 }
 </style>
